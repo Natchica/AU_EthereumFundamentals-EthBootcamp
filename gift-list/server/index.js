69bb0f829d893b6faa0f0dec63c1d04b9a88228e
Natchica <nathan.gaud@gmail.com>
2024-11-27 16:40:46 +0100

    Week 2 project work

---
+++ gift-list/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+const MERKLE_ROOT = "501a5e32ee39d82852224eb456a52b8165a3a8282d140ebb81f96d296f733f15";
+
+app.post('/gift', (req, res) => {
+  const { name, proof } = req.body;
+
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
