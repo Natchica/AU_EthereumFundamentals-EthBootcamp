5fc7cc0703c35d0f8108d593da0d6ab65289f231
Natchica <nathan.gaud@gmail.com>
2024-12-03 18:51:09 +0100

    Week 3 project code

---
+++ alchemyu-json-activity/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+// get the content of the alchemy_url.txt file
+const ALCHEMY_URL = fs.readFileSync('alchemy_url.txt', 'utf8').trim();
+
+axios.post(ALCHEMY_URL, {
+    "id": 1,
+    "jsonrpc": "2.0",
+    "params": [
+      "0xb86120f0eDc5F32E6D2151d6FC4FCb3c0c2f537D",
+      "latest"
+    ],
+    "method": "eth_getBalance"
+}).then((response) => {
+  console.log(response.data.result);
+});
